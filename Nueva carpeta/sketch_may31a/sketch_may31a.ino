5dabf7030cbc5cc10ea3c89e854508550a11c09f
brayamdonayreMERS <134813636+brayamdonayreMERS@users.noreply.github.com>
2023-05-31 20:43:03 -0500

    Variables
    Asignacion de variables para los pin y otras variables como tiempo y distancia

---
+++ Nueva carpeta/sketch_may31a/sketch_may31a.ino
@@ -0,0 +1,10 @@
+//Primera practica para sistema de seguridad con arduino
+//Asignamos variales para los nuevos pin 
+int trig=2;
+int echo=3;
+int distancia;
+int tiempo;
+int pir=4;
+int motor=5;
+int valorLDR=0;
+int pinLDR=A0;
